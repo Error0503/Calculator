0a28e320dc337325e34b93d9d34ff5145850971b
Error0503 <vargha.simon@gmail.com>
2021-07-23 01:54:12 +0200

    All C(lear) and = buttons implemented. Buttons resized. Text size increased. Window resized.

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -6,85 +6,18 @@ namespace Calculator
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private Label l;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            //this.label1 = new System.Windows.Forms.Label();
-            //this.button = new System.Windows.Forms.Button();
-            //this.SuspendLayout();
 
-
-
-            //this.label1.AutoSize = true;
-            //this.label1.Location = new System.Drawing.Point(354, 225);
-            //this.label1.Name = "label1";
-            //this.label1.Size = new System.Drawing.Size(35, 13);
-            //this.label1.TabIndex = 0;
-            //this.label1.Text = "label1";
-
-
-
-            //this.button.AutoSize = true;
-            //this.button.Location = new System.Drawing.Point(100, 100);
-            //this.button.Name = "button";
-            //this.button.Size = new System.Drawing.Size(75, 23);
-            //this.button.TabIndex = 0;
-            //this.button.Text = "I\'m a button";
-            //this.button.Click += new System.EventHandler(this.button_Click);
-
-
-
-
-            /*
-            for (int y = 2; y >= 0; y--)
-            {
-                for (int x = 2; x >= 0; x--)
-                {
-                    System.Windows.Forms.Button b = new Button();
-                    b.AutoSize = true;
-                    b.Size = new System.Drawing.Size(40, 40);
-                    b.Location = new System.Drawing.Point(x * 40, y * 40);
-                    b.Name = (x + y * 3) + 1 + "button";
-                    b.TabIndex = (x + y * 3);
-                    b.Text = (x + y * 3) + 1 + "";
-                    b.Anchor = AnchorStyles.Top;
-                    b.Click += new System.EventHandler(button_Click);
-                    Console.WriteLine("Button " + (x + y * 3) + 1 + " created");
-                    Controls.Add(b);
-                }
-
-            }
-            */
-
-            Label l = new Label();
+            l = new Label();
             l.Location = new System.Drawing.Point(0, 0);
-            l.Size = new System.Drawing.Size(4 * 40, 40);
+            l.Size = new System.Drawing.Size(4 * 80, 80);
             l.Name = "label";
-            l.Text = "LABEL";
+            l.Text = "";
             l.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            l.Font = new System.Drawing.Font("Arial", 30, System.Drawing.FontStyle.Regular);
             Controls.Add(l);
 
 
@@ -95,8 +28,8 @@ namespace Calculator
                 {
                     Button b = new Button();
                     b.AutoSize = true;
-                    b.Size = new System.Drawing.Size(40, 40);
-                    b.Location = new System.Drawing.Point(x * 40, y * 40);
+                    b.Size = new System.Drawing.Size(80, 80);
+                    b.Location = new System.Drawing.Point(x * 80, y * 80);
                     b.Name = n + "button";
                     b.Text = n + "";
                     b.Click += new System.EventHandler(button_Click);
@@ -108,29 +41,33 @@ namespace Calculator
                 n -= 6;
             }
 
-            Button b0 = new Button();
-            b0.AutoSize = true;
-            b0.Size = new System.Drawing.Size(40, 40);
-            b0.Location = new System.Drawing.Point(1 * 40, 4 * 40);
-            b0.Name = "0button";
-            b0.Text = "0";
-            b0.Click += new System.EventHandler(button_Click);
-            Console.WriteLine("Button 0 created");
+            String[] symbols = { "C", "0", "=" };
+            for (int x = 0; x < 3; x++)
+            {
+                Button b = new Button();
+                b.AutoSize = true;
+                b.Size = new System.Drawing.Size(80, 80);
+                b.Location = new System.Drawing.Point(x * 80, 4 * 80);
+                b.Name = symbols[x] + "button";
+                b.Text = symbols[x] + "";
+                b.Click += new System.EventHandler(button_Click);
+                Console.WriteLine("Button " + symbols[x] + " created");
 
-            Controls.Add(b0);
+                Controls.Add(b);
+            }
 
-            String[] symbols = { "+", "-", "×", "÷" };
 
+            String[] symbols2 = { "+", "-", "×", "÷" };
             for (int y = 1; y < 5; y++)
             {
                 Button b = new Button();
                 b.AutoSize = true;
-                b.Size = new System.Drawing.Size(40, 40);
-                b.Location = new System.Drawing.Point(3 * 40, y * 40);
-                b.Name = symbols[y - 1] + "button";
-                b.Text = symbols[y - 1] + "";
+                b.Size = new System.Drawing.Size(80, 80);
+                b.Location = new System.Drawing.Point(3 * 80, y * 80);
+                b.Name = symbols2[y - 1] + "button";
+                b.Text = symbols2[y - 1] + "";
                 b.Click += new System.EventHandler(button_Click);
-                Console.WriteLine("Button " + symbols[y - 1] + " created");
+                Console.WriteLine("Button " + symbols2[y - 1] + " created");
 
                 Controls.Add(b);
             }
@@ -144,24 +81,26 @@ namespace Calculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(4 * 80, 5 * 80);
 
 
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Calculator";
+            this.Text = "Calculator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
-        #endregion
-
-        //private System.Windows.Forms.Label label1;
-        //private System.Windows.Forms.Button button;
-
         private void button_Click(object sender, System.EventArgs e)
         {
-            MessageBox.Show(((Button)sender).Name + " was clicked");
+            if (((Button)sender).Name.Equals("Cbutton"))
+            {
+                l.Text = "";
+            }
+            else
+            {
+                l.Text += ((Button)sender).Text;
+            }
         }
     }
 }
