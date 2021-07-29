242d7e4100f93860be7e9d80c361ca40536eaa73
Error0503 <vargha.simon@gmail.com>
2021-07-29 10:40:53 +0200

    Button functions added

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -12,6 +12,10 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+
+        private int num1, num2;
+        private char operation;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,9 +26,67 @@ namespace Calculator
 
         }
 
-        private void button_Click(object sender, EventArgs e)
+        private void EvalButton_Click(object sender, EventArgs e)
         {
+            try
+            {
+                num2 = int.Parse(display.Text);
+                long result = 0;
+                switch (operation)
+                {
+                    case '+':
+                        result = num1 + num2;
+                        display.Text = result + "";
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        display.Text = result + "";
+                        break;
+                    case '×':
+                    case '*':
+                        result = num1 * num2;
+                        display.Text = result + "";
+                        break;
+                    case '÷':
+                    case '/':
+                        if (num2 != 0)
+                        {
+                            double res = (double)num1 / num2;
+                            res = Math.Round(res, 10);
+                            display.Text = res + "";
+                        }
+                        else
+                        {
+                            display.Text = "Err";
+                        }
+                        break;
+                }
+                if (result > int.MaxValue) display.Text = "Err";
+                num1 = int.Parse(display.Text);
+            }
+            catch (FormatException) { }
+            catch (OverflowException) { display.Text = "Err"; }
+        }
 
+        private void OpButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                num1 = Int32.Parse(display.Text);
+            }
+            catch (FormatException) { }
+            operation = Char.Parse(((Button)sender).Text);
+            display.Text = "";
+        }
+
+        private void CButton_Click(object sender, EventArgs e)
+        {
+            display.Text = "";
+        }
+
+        private void NumButton_Click(object sender, EventArgs e)
+        {
+            display.Text += ((Button)sender).Text;
         }
 
         private void label1_Click(object sender, EventArgs e)
